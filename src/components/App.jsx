51f43974d0479d626e3433c67c3ec13aab670109
myle-truong <56028880+myle-truong@users.noreply.github.com>
2020-02-05 08:47:42 +0100

    add backgound colors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Login from './Login';
 
 const isLoggedIn = false;
 
-//check login condition
-function renderConditionally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+const currentTime = new Date('Wed Feb 06 2020 02:07:00 GMT+0530').getHours();
+console.log(currentTime);
 
-function App() {
-  return <div className="container">{renderConditionally()}</div>;
+//check login condition
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      backgroundColor: ''
+    };
+  }
+
+  // Auto color run depend on current time
+
+  backgroundColors = () => {
+    if (currentTime >= 23 && currentTime < 7) {
+      this.setState({
+        backgroundColor: 'red'
+      });
+    }
+    if (currentTime > 7 && currentTime < 14) {
+      this.setState({
+        backgroundColor: 'blue'
+      });
+    } else {
+      this.setState({
+        backgroundColor: 'green'
+      });
+    }
+    console.log(this.state.backgroundColor);
+  };
+
+  //check conditional login
+  renderConditionally = () => {
+    if (isLoggedIn === true) {
+      return <h1>Hello</h1>;
+    } else {
+      return <Login backgroundColors={this.backgroundColors} />;
+    }
+  };
+  render() {
+    return <div className="container">{this.renderConditionally()}</div>;
+  }
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Input from './Input';
 
-function Login() {
+const Login = ({ props }) => {
+  //console.log(backgroundColors);
   return (
-    <form className="form">
+    <form className="form" style={{ backgroundColor: props }}>
       <Input type="text" placeholder="Username" />
       <Input type="password" placeholder="Password" />
       <button type="submit">Login</button>
     </form>
   );
-}
+};
 
 export default Login;
