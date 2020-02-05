29bcdcf5595d8379ec32e692f6e1df1ce3b7db1b
myle-truong <56028880+myle-truong@users.noreply.github.com>
2020-02-05 15:40:35 +0100

    add registerform

---
+++ src/backgoundchange.js
@@ -0,0 +1,36 @@
+// import React, { useState, useEffect } from 'react';
+// import Login from './Login';
+
+// const App = () => {
+//   const [background, setBackground] = useState('white');
+//   const [currentTime, setCurrentTime] = useState(new Date().getHours() + 10);
+//   console.log(currentTime);
+//   let style = {
+//     backgroundColor: background
+//   };
+//   const handleClick = () => {
+//     setBackground('red');
+//   };
+
+//   useEffect(() => {
+//     const changeColor = () => {
+//       if (currentTime >= 0 && currentTime < 9) {
+//         setBackground('red');
+//       } else if (currentTime > 9 && currentTime < 15) {
+//         setBackground('blue');
+//       } else if (currentTime > 16) {
+//         setBackground('green');
+//       }
+//     };
+//     changeColor();
+//   }, []);
+
+//   //check conditional login
+
+//   return (
+//     <div id="app" onClick={handleClick} style={style}>
+//       <h3>App</h3>
+//     </div>
+//   );
+// };
+// export default App;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import Login from './Login';
+import React from 'react';
+import Form from './Form';
 
-const App = () => {
-  const [background, setBackground] = useState('white');
-  const [currentTime, setCurrentTime] = useState(new Date().getHours() + 10);
-  console.log(currentTime);
-  let style = {
-    backgroundColor: background
-  };
-  const handleClick = () => {
-    setBackground('red');
-  };
+const userIsRegistered = true;
 
-  useEffect(() => {
-    const changeColor = () => {
-      if (currentTime >= 0 && currentTime < 9) {
-        setBackground('red');
-      } else if (currentTime > 9 && currentTime < 15) {
-        setBackground('blue');
-      } else if (currentTime > 16) {
-        setBackground('green');
-      }
-    };
-    changeColor();
-  }, []);
-
-  //check conditional login
-
+function App() {
   return (
-    <div id="app" onClick={handleClick} style={style}>
-      <h3>App</h3>
+    <div className="container">
+      <Form />
     </div>
   );
-};
+}
+
 export default App;
